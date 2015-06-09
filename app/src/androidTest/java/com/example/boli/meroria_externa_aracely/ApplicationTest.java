9120c47385257d756c7e3d365ce3807c5522607f
Aracely <camp_nita@hotmail.com>
2015-06-09 11:12:29 -0500

    Version 1 Meoria Externa

---
+++ app/src/androidTest/java/com/example/boli/meroria_externa_aracely/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.boli.meroria_externa_aracely;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
